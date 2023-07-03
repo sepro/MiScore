718da68b6f0aeda81ff3a29a90ca7a132a0c498c
Sebastian Proost <sebastian.proost@gmail.com>
2023-07-03 09:51:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="miscore",
-    version="0.0.1",
+    version="0.0.2",
     author="Sebastian Proost",
     author_email="sebastian.proost@gmail.com",
     description="Package to manage high scores in JSON format",
@@ -15,7 +15,7 @@ setuptools.setup(
     project_urls={
         "Bug Tracker": "https://github.com/sepro/MiScore/issues",
     },
-    install_requires=["click>=8.1.3", "pydantic>=1.9.2"],
+    install_requires=["click>=8.1.3", "pydantic>=1.9.2,<=2.0.0"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
